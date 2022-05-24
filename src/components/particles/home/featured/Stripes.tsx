08de8f9666d8eb99e29a703fd61bfaaba5d2967d
victor benavente <templario0141@gmail.com>
2022-05-24 00:31:23 -0500

    feat: add ease polyout animation

--- src/components/particles/home/featured/Stripes.tsx
+++ src/components/particles/home/featured/Stripes.tsx
@@ -1,1 +1,80 @@
-export const a = null
+import React, { Component } from 'react'
+import { Animate } from 'react-move'
+import { easePolyOut } from 'd3-ease'
+
+export default class Stripes extends Component {
+  state = {
+    stripes: [
+      {
+        background: '#98c5e9',
+        left: 120,
+        rotate: 25,
+        top: -260,
+        delay: 0,
+      },
+      {
+        background: '#fffff',
+        left: 360,
+        rotate: 25,
+        top: -297,
+        delay: 200,
+      },
+      {
+        background: '#98c5e9',
+        left: 600,
+        rotate: 25,
+        top: -498,
+        delay: 400,
+      },
+    ],
+  }
+
+  showStripes = () => {
+    return this.state.stripes.map((stripe, i) => (
+      <Animate
+        key={i}
+        show={true}
+        start={{
+          background: '#ffffff',
+          opacity: 0,
+          left: 0,
+          rotate: 0,
+          top: 0,
+        }}
+        enter={{
+          background: [stripe.background],
+          opacity: [1],
+          left: [stripe.left],
+          rotate: [stripe.rotate],
+          top: [stripe.top],
+          timing: {
+            delay: stripe.delay,
+            duration: 200,
+            ease: easePolyOut,
+          },
+          events: {
+            end(){
+              console.log('animation finish')
+            }
+          }
+        }}
+      >
+        {({ opacity, left, rotate, top, background }) => {
+          return (
+            <div
+              className="stripe"
+              style={{
+                background: background as string,
+                opacity: opacity as number,
+                transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`,
+              }}
+            ></div>
+          )
+        }}
+      </Animate>
+    ))
+  }
+  render() {
+    return <div className="featured_stripes">{this.showStripes()}</div>
+  }
+}
--- src/components/particles/home/featured/index.tsx
+++ src/components/particles/home/featured/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import  Stripes from './Stripes'
 
 export const Featured: React.FC<any> = () => {
-  return <div className="featured_wrapper">Some text</div>
+  return <div className="featured_wrapper">
+    <Stripes></Stripes>
+  </div>
 }
